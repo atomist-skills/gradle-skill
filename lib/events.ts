b3a26f48acf59ee1310d06ebcb9baff70b9d63f7
Christian Dupuis <cd@atomist.com>
2020-12-04 16:58:21 +0100

    Polish

--- lib/events.ts
+++ lib/events.ts
@@ -33,7 +33,7 @@ import { extractAnnotations } from "./annotation";
 import { tokenizeArgString } from "./args";
 import { Configuration } from "./configuration";
 import { eventCommit, eventRepo } from "./git";
-import { spawnFailure, statusReason } from "./status";
+import { spawnFailure, statusReason, trimDirectory } from "./status";
 
 interface GradleParameters {
 	project: project.Project;
@@ -133,7 +133,9 @@ const CommandStep: GradleStep = {
 				}),
 			);
 		}
-		params.body.push(`Setup command \`${result.cmdString}\` successful`);
+		params.body.push(
+			`Setup command \`${trimDirectory(result.cmdString)}\` successful`,
+		);
 		await params.check.update({
 			conclusion: undefined,
 			body: params.body.join("\n\n---\n\n"),
@@ -249,7 +251,7 @@ const GradleGoalsStep: GradleStep = {
 				}),
 			);
 		}
-		params.body.push(`\`${result.cmdString}\` successful`);
+		params.body.push(`\`${trimDirectory(result.cmdString)}\` successful`);
 		await params.check.update({
 			conclusion: "success",
 			body: params.body.join("\n\n---\n\n"),
