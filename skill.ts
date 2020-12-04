d237d473acb1491ef4d2b2706e32a9ef0940b988
Christian Dupuis <cd@atomist.com>
2020-12-04 14:46:59 +0100

    Change default cache pattern

--- skill.ts
+++ skill.ts
@@ -136,7 +136,7 @@ export const Skill = skill<
 			description:
 				"Cache and restore file system content between executions of this skill",
 			required: false,
-			defaultValue: [".gradle/caches/**"],
+			defaultValue: [".gradle/**"],
 			visibility: ParameterVisibility.Advanced,
 		},
 		repos: parameter.repoFilter(),
